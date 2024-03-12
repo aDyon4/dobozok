87c386d714e3a88f156d825b25a1cdfd0b0a197e
Adam Dominik <12adamdominik@gmail.com>
2024-03-12 13:44:19 +0100

    Kattint

--- src/main/java/com/example/dobozok/HelloController.java
+++ src/main/java/com/example/dobozok/HelloController.java
@@ -11,9 +11,13 @@ import javafx.scene.layout.Pane;
 public class HelloController {
     @FXML private Pane pnJatek;
     @FXML private ImageView ivNyil;
+    @FXML private Label lbZart;
+    @FXML private Label lbNyitott;
 
     private Label[][] lt = new Label[10][15];
     private String[][] t = new String[10][15];
+    private int zartDb = 0;
+    private int nyitottDb = 0;
 
     private AnimationTimer timer = null;
     int f = 0;
@@ -22,18 +26,19 @@ public class HelloController {
     Image box =new Image(getClass().getResourceAsStream("box.png"));
     Image boxopen =new Image(getClass().getResourceAsStream("boxopen.png"));
     public void initialize(){
-
+        lbZart.setText(zartDb + " db");
+        lbNyitott.setText(nyitottDb + " db");
         for(int s = 0;s<10;s++){
             for(int o = 0;o<15;o++){
                 int ss = s, oo = o;
                 t[s][o] = "null";
                 lt[s][o] = new Label("");
                 lt[s][o].setGraphic(new ImageView(nulll));
-                random();
                 lt[s][o].setLayoutX(10+o*64);
                 lt[s][o].setLayoutY(10+s*64);
                 lt[s][o].setOnMouseEntered(e -> lt[ss][oo].setStyle("-fx-background-color: lightgreen"));
                 lt[s][o].setOnMouseExited(e -> lt[ss][oo].setStyle("-fx-background-color: white"));
+                lt[s][o].setOnMousePressed(e -> katt(ss,oo));
                 pnJatek.getChildren().add(lt[s][o]);
             }
         }
@@ -42,6 +47,7 @@ public class HelloController {
             public void handle(long now) {
                 f++;
                 forgat();
+                torol();
             }
         };
         timer.start();
@@ -66,16 +72,35 @@ public class HelloController {
         for(int o = 0;o<15;o++){
             lt[9-rt[o]][o].setGraphic(new ImageView(box));
             t[9-rt[o]][o] = "box";
+
         }
         for(int s = 0;s<10;s++){
             for(int o = 0;o<15;o++){
-                if(t[s][o].equals("box")) { lt[s+1][o].setGraphic(new ImageView(box)); t[s+1][o] = "box"; }
+                int ss = s, oo = o;
+                if(t[s][o].equals("box")) {
+                    lt[s+1][o].setGraphic(new ImageView(box));
+                    t[s+1][o] = "box";
+                    zartDb++;
+                }
+
             }
         }
+        lbZart.setText(zartDb + " db");
     }
 
-    public void random(){
-       
+    int fs = 0, fo=0;
+    private void katt(int s, int o){
+        if(t[s-1][o].equals("null") && t[s][o].equals("box")) { lt[s][o].setGraphic(new ImageView(boxopen)); t[s][o] = "boxopen"; nyitottDb++; zartDb--;}
+        else if(t[s+1][o].equals("box")) { lt[s][o].setGraphic(new ImageView(box)); t[s][o] = "box"; zartDb++;}
+        fs = s;
+        fo = o;
+
+        lbZart.setText(zartDb + " db");
+        lbNyitott.setText(nyitottDb + " db");
+        torol();
+    }
+    private void torol(){
+        //lt[fs][fo].setGraphic(new ImageView(nulll)); t[fs][fo] = "null";
     }
 
 }
